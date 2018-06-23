4df04e7af92d6ed260bc8ddf16d782ad93f57258
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-23 12:55:28 +0300

    create ui class. display weater object in index.html ui

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // init weather object
 const weather = new Weather('Boston', 'MA');
+// init ui
+const ui = new UI();
 
 // get weather on dom load
-document.addEventListener('DOMContetntLoaded', getWeather);
+//document.addEventListener('DOMContetntLoaded', getWeather);
+getWeather();
 
 function getWeather(){
     weather.getWeather()
     .then(results => {
-        console.log(results);
+        ui.paint(results);
     })
     .catch(err => console.log(err));
 }
