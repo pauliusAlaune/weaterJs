915b43062d0a2c23f99421a59cb9ad6fcd5aaa1a
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-23 14:28:29 +0300

    Option to change cities and state. Save location to local storage. bug fix to index.html

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// init storage
+const storage = new Storage();
+// get stored location data
+const weatherLocation = storage.getLocationData();
 // init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 // init ui
 const ui = new UI();
 
 // get weather on dom load
 //document.addEventListener('DOMContetntLoaded', getWeather);
 getWeather();
+
+// change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    
+    // change location
+    weather.changeLocation(city, state);
+    
+    // set location in ls
+    storage.setLocationData(city, state);
+    
+    // get and display weather
+    getWeather();
+    
+    // close modal
+    $('#locModal').modal('hide');
+});
+
 
 function getWeather(){
     weather.getWeather()
